05eb3e8feb9660b64450f5259114a9450af09e3f
Aryavrat Mishra <ym940828@gmail.com>
2026-09-10 19:00:22 +0530

    fixes

--- resources/views/vpn-success.blade.php
+++ resources/views/vpn-success.blade.php
@@ -62,7 +62,7 @@
             </div>
             <div class="flex items-start gap-3">
                 <span class="text-blue-400 mt-0.5">②</span>
-                <span>CITC team verifies your details .</span>
+                <span>CITC team will verify your details .</span>
             </div>
             <div class="flex items-start gap-3">
                 <span class="text-blue-400 mt-0.5">③</span>
